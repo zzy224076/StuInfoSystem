dc93f8ddc5a90f5f45519244576de251c4f335ae
zzy224076 <2240769410@qq.com>
2021-06-26 21:54:51 +0800

    增加了一个底部导航栏、更改了student实体类

---
+++ app/src/main/java/com/example/stuinfosystem/StuActivity.kt
@@ -0,0 +1,35 @@
+package com.example.stuinfosystem
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.stuinfosystem.databinding.ActivityStuBinding
+
+class StuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityStuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_stu)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/example/stuinfosystem/Student.kt
+++ app/src/main/java/com/example/stuinfosystem/Student.kt
@@ -3,14 +3,19 @@ package com.example.stuinfosystem
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.lang.ClassCastException
 
 @Entity(tableName = "Student")
 data class Student(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey()
     var studentID:Int,
     @ColumnInfo(name = "s_name")
     var stuName: String,
     @ColumnInfo(name = "s_sex")
-    var stuSex:String
+    var stuSex:String,
+    @ColumnInfo(name = "stu_tel")
+    var stuTel:String?,
+    @ColumnInfo(name = "stu_class")
+    var stuClass:String?
 
 )
